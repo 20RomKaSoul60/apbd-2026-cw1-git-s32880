9a5d4f80b018cbeda3e19417067165456d463ece
20RomKaSoul <s32880@pjwstk.edu.pl>
2026-03-13 08:55:24 +0100

    Creation of function Calculate AVG

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+int CalclateAVG(int[] arr)
+{
+    int tmp = 0;
+
+
+    return tmp;
+
+
+}
