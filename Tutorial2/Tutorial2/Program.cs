87d41541785a171f9a0fa5caa1d972698d3b834a
s32880 <s32880@pjwstk.edu.pl>
2026-03-13 09:32:40 +0100

    Modified first WriteLine in Program.cs

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Creating new math funcs");
 
 int CalclateAVG(int[] arr)
 {
